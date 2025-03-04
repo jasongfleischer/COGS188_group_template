98163f19d1594f55337ec33894693dbdc405ce18
yozygao <yozygao@gmail.com>
2025-03-04 03:37:47 -0800

    Zzzzzzzzzzzzzzzz

---
+++ monte_carlo_try.py
@@ -0,0 +1,8 @@
+import numpy as np
+import random
+from collections import defaultdict
+import mahjong_environment
+
+def __init__(self, env: mahjong_environment, gamma: float = 1.0, epsilon: float = 0.1, Q0: float = 0.0, max_episode_size : int = 1000):
+
+
