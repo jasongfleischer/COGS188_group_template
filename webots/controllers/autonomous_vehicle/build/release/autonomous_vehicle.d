bf6214567862b2b4c6a9572fdde2007a3722c4a9
Jiawei Gao <jiaweigao@MacBook-Pro.attlocal.net>
2025-03-16 17:25:05 -0700

    incomplete camera algorithm

---
+++ webots/controllers/autonomous_vehicle/build/release/autonomous_vehicle.d
@@ -0,0 +1,13 @@
+build/release/autonomous_vehicle.o: autonomous_vehicle.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/camera_recognition_object.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/device.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/display.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/gps.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/keyboard.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/lidar.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/lidar_point.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/vehicle/driver.h
